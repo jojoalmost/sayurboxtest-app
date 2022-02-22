03d32db0a34597e6fb20cba62ee8bf9d33fe3dcf
Yosua Risandy <jojo@kata.ai>
2022-02-22 19:24:24 +0700

    add setry log

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserTracing} from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://dc722430570d440381308e5a64a90ffe@o1149190.ingest.sentry.io/6223244",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
